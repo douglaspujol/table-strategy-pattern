ddc3983e37df8920fa02365cb463271fc6c5eddd
Douglas Pujol <douglas.pujol@orbia.ag>
2024-11-03 12:51:05 -0300

    feat: input search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -75,6 +75,30 @@ export default function Home() {
           </li>
         ))}
       </ul>
+      <div className="mb-6 p-2 w-auto inline-flex items-center border border-dashed border-border focus-within:border-hoverContentTable hover:border-hoverContentTable group">
+        <input 
+          type="search" 
+          className="w-72  text-font text-base bg-transparent focus:outline-none" 
+          placeholder="Enter name or number to search"
+        />
+        <div>
+          <svg 
+            className="h-4 text-font group-hover:text-hoverContentTable group-focus-within:text-hoverContentTable" 
+            aria-hidden="true" 
+            xmlns="http://www.w3.org/2000/svg" 
+            fill="none" 
+            viewBox="0 0 20 20"
+          >
+            <path 
+              stroke="currentColor" 
+              strokeLinecap="round" 
+              strokeLinejoin="round" 
+              strokeWidth="2" 
+              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" 
+            />
+          </svg>
+        </div>
+      </div>
 
       <Table
         strategy={strategy}
